d4c2778fbfa7d004f310a83fc0170c8f4a810140
StupidWebsites <85293798+stupidsites@users.noreply.github.com>
2021-10-06 21:24:22 +0200

    Create pop.js

---
+++ js/pop.js
@@ -0,0 +1,28 @@
+var myObject = {
+    "name": "AstroSwap",
+    "ticker": "$ASTRO",
+    "addr": "0xa3eF51eB4942116064F3659D97Cea22462A56077",
+    "price": "0.001",
+    "ethamount": "1666",
+    "usdtamount": "5.55",
+    "total": "700,000",
+    "raised": "560,133",
+    "softcap": "168,309",
+    "percent": "80%",
+    "mineth": "0.22",
+    "maxeth": "5",
+	"minbnb": "1.31",
+	"maxbnb": "25",
+    "minusdt": "621",
+	
+	"minbnbnow": "2.61",
+    "minethnow": "0.51",
+    "minusdtnow": "951",
+	
+    "maxusdt": "21444",
+    "telegram": "https://t.me/gamezoneapp",
+    "url": "https://xion.finance",
+    "logo": "https://nftlaunch.network/img/logo.png",
+    "favicon": "favicon.ico",
+};
+w3.displayObject("contentw3", myObject);
